ad3154c703ea74db24732b149fd66522f96e2edf
NICO :D <nicopereyra855@gmail.com>
2025-12-21 10:24:35 -0300

    assitencias al ping

--- frontend/const/tabs.tsx
+++ frontend/const/tabs.tsx
@@ -7,7 +7,6 @@ import TrendingDownIcon from '@mui/icons-material/TrendingDown'
 import SportsHandballIcon from '@mui/icons-material/SportsHandball'
 import MailOutlineIcon from '@mui/icons-material/MailOutline'
 import BarChartIcon from '@mui/icons-material/BarChart'
-import TrendingUpIcon from '@mui/icons-material/TrendingUp'
 
 export const recepcionistTabs = [
     { label: "Miembros", icon: <GroupIcon />, value: "members" },
@@ -25,7 +24,6 @@ export const adminTabs = [
   { label: "Miembros", icon: <GroupIcon />, value: "members" },
   { label: "Pagos", icon: <AttachMoneyIcon />, value: "shift-payments" },
   { label: "Deudas", icon: <ReceiptLongIcon />, value: "deudas" },
-  { label: "Asistencias", icon: <TrendingUpIcon />, value: "assists" },
   { label: "Planes", icon: <FitnessCenterIcon />, value: "plans" },
   { label: "Turnos", icon: <ScheduleIcon />, value: "shifts" },
   { label: "Egresos", icon: <TrendingDownIcon />, value: "egresos" },
